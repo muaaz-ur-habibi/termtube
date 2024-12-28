f08156feb57b5cee880b3091b523ecb4a6b9ec08
Muaaz <152279555+muaaz-ur-habibi@users.noreply.github.com>
2024-12-28 12:20:11 +0500

    added audio support

--- video_player.py
+++ video_player.py
@@ -1,111 +1,76 @@
 import cv2
 import os
-import re
-from ascii_magic import AsciiArt
 import time
-
+import sys
+from moviepy import AudioFileClip
+import pygame.mixer as mixer
 from PIL import Image
+
 
 
 CONSOLE_COLUMNS = os.get_terminal_size().columns
 if CONSOLE_COLUMNS % 2 != 0:
     CONSOLE_COLUMNS-=1
+CONSOLE_ROWS = os.get_terminal_size().lines
 
 
 class Player:
-    def __init__(self, video_file_path, audio_file_path, video_save_path, frames_save_path):
+    def __init__(self, video_file_path, audio_file_path):
         self.video_path = video_file_path
         self.audio_path = audio_file_path
-        self.save_path = video_save_path
-        self.frames_path = frames_save_path
-    
-    def sorted_alphanumeric(self, data):
-        convert = lambda text: int(text) if text.isdigit() else text.lower()
-        alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-        return sorted(data, key=alphanum_key)
 
-    def split_video_into_frames(self):
-        cap = cv2.VideoCapture(self.video_path)
+        mixer.init()
 
-        succ, img = cap.read()
-        c = 1
-        while succ:
-            cv2.imwrite(f"{self.save_path}/frame_{c}.jpg", img)
-
-            succ, img = cap.read()
-
-            c+=1
-    
-    def set_framerate(self, rate:int):
-        # sets the framerate by deleting rate number of frames
-        # starting from frame_1
-
-        contents = self.sorted_alphanumeric(os.listdir(self.save_path))
-
-        # save only the frames that are needed as per the rate
-        for i in range(1, len(contents), rate):
-            open(f"{self.frames_path}/{contents[i]}", "a").close()
-
-            f_w = open(f"{self.frames_path}/{contents[i]}", "wb")
-            f_r = open(f"{self.save_path}/{contents[i]}", "rb")
-
-            f_w.write(f_r.read())
-
-            f_w.close()
-            f_r.close()
-        
-        # cleaning up aka deleting all the initial frames
-        for file in contents:
-            f = f"{self.save_path}/{file}"
-            #os.remove(f)
-    
-    def create_ascii_text(self):
-        contents = self.sorted_alphanumeric(os.listdir(self.frames_path))
-
-        for f in contents:
-            file = f"{self.frames_path}/{f}"
-            
-            art = AsciiArt.from_image(file)
-            
-            art.to_terminal(columns=CONSOLE_COLUMNS, width_ratio=2)
+    def convert_mp4_to_wav(self):
+        mp4 = AudioFileClip(self.video_path)
+        mp4.write_audiofile(self.audio_path, logger=None)
+        mp4.close()
     
     def scale(self, img:Image.Image, width):
-        (og_w, og_h) = img.size
-        ratio = og_h/float(og_w)
+        #w = (CONSOLE_ROWS/img.height) * (img.width/img.height) * 2.5
 
-        height = int(ratio * width / 2)
+        #w = int(CONSOLE_COLUMNS * w)
+        w = CONSOLE_COLUMNS
 
-        new_img = img.resize((width, height))
+        new_img = img.resize((w, CONSOLE_ROWS))
+        #new_img = cv2.resize(img, (0, 0), fx=CONSOLE_COLUMNS/og_w, fy=CONSOLE_ROWS/og_h)
         return new_img
     
     def map_pixel_to_ascii(self, img:Image.Image, width=CONSOLE_COLUMNS):
         pixels = img.getdata()
-
         ascii_str = ""
-        range_width = 25.5
+        range_width = 255/(len(self.CHARS)-1.001)
 
-        i = 0
-
+        i = 1
+        
         for pixel_val in pixels:
             intensity = (0.299 * pixel_val[0] + 0.587 * pixel_val[1] + 0.114 * pixel_val[2])
+            #intensity = ((pixel_val[0] + pixel_val[1] + pixel_val[2])/3)
 
             index = int(intensity//range_width)
 
             if index >= len(self.CHARS):
-                index = len(self.CHARS) -1
-            
-            ascii_str += self.CHARS[index]
+                index = len(self.CHARS)
+
+            ascii_str += f'\x1b[38;2;{int(pixel_val[2])};{int(pixel_val[1])};{int(pixel_val[0])}m{self.CHARS[index]}'
+            #ascii_str += self.CHARS[index]
 
             if i % width == 0:
                 ascii_str += '\n'
+                i = 0
             
             i+=1
 
         return ascii_str
+    
+    def clear_screen(self):
+        sys.stdout.write(f"\033[{CONSOLE_ROWS}F")
 
-    def ASCII(self, img_path, width):
-        img = Image.open(img_path)
-        self.CHARS = '.:-=+*#%@'
+    def ASCII(self, frame, width):
+        img = Image.fromarray(frame)
+
+        self.CHARS = ' .-=:+"*#%@'
+        #self.CHARS = '@%#*"x+=-:. '
 
         image = self.scale(img, width)
         art = self.map_pixel_to_ascii(image)
@@ -113,22 +78,53 @@ class Player:
         return art
     
     def PLAY(self, w):
-        frames = self.sorted_alphanumeric(os.listdir(self.frames_path))
+        #self.player = MediaPlayer(self.audio_path)
+        mixer.music.load(self.audio_path)
+        
+        #a, v = self.player.get_frame()
 
-        for frame in frames:
-            art = self.ASCII(f"{self.frames_path}/{frame}", width=w)
+        vid = cv2.VideoCapture(self.video_path)
+        fps = vid.get(cv2.CAP_PROP_FPS)
+
+        fps = 1/fps
+
+        try:
+            succ = True
+
+            mixer.music.play()
             
-            print(art)
+            while succ:
+                current_time = time.time()
+                succ, img = vid.read()
+
+                #self.player.set_pause(False)
+                mixer.music.unpause()
+
+                art = self.ASCII(
+                    img,
+                    width=w)
+
+                sys.stdout.write(art[:-1])
+                #print(art[:-1], end="")
+                
+                while True:
+                    if time.time() - current_time < fps:
+                        pass
+                    else:
+                        self.clear_screen()
+                        #self.player.set_pause(True)
+                        mixer.music.pause()
+                        break
+                
+        except KeyboardInterrupt:
+            print("Interrupt detected while playing video")
+
 
 if __name__ == "__main__":
     print("starting player")
-    plyr = Player("temp/COPY.mp4", "temp/curr_aud.mp3", "player_temp", "player_frames")
-    
-    fps = 1
-    
-    #plyr.split_video_into_frames()
-    #plyr.set_framerate(fps)
+    plyr = Player("temp/curr_vid.mp4", "temp/curr_aud.mp3")
+    plyr.convert_mp4_to_wav()
 
-    plyr.PLAY()
+    plyr.PLAY(CONSOLE_COLUMNS)
 
     print("player ended")
